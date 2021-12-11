59a738b95cbbc9b5ce60a8a23113191843debbd8
Hacker Jr <70173542+HackerX7@users.noreply.github.com>
2021-12-11 21:35:13 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,6 +1,5 @@
 class script(object):
     START_TXT = """Hello {},
-
 My Name , <a href='https://t.me/Maakri_filter_bot'>Millie ✨🕊</a>,
 It's Very Easy Just Add Me To Your Group And Make Me Admin, That's All I'll Provide Movies There 🤓
 """
@@ -8,8 +7,8 @@ It's Very Easy Just Add Me To Your Group And Make Me Admin, That's All I'll Prov
 
 Here Is The Help For My Commands."""
     ABOUT_TXT =  """<b>⪼ Mʏ Nᴀᴍᴇ : <a href='https://t.me/Maakri_filter_bot'>Millie ✨🕊</a>
-⪼ Dᴇᴠᴇʟᴏᴘᴇʀ 1: <a href='https://t.me/Hacker_Jr'>HᴀᴄKᴇʀ Jʀ 🇮🇳 / 🇺🇸</a>
-⪼ Dᴇᴠᴇʟᴏᴘᴇʀ 2: <a href='https://t.me/blesson_3'>𝐌𝐑 𝐌𝐀𝐊𝐑𝐈</a>
+⪼ Dᴇᴠᴇʟᴏᴘᴇʀ 1: <a href='https://t.me/blesson_3'>Mʀ Mᴀᴋʀɪ</a>
+⪼ Dᴇᴠᴇʟᴏᴘᴇʀ 2: <a href='https://t.me/Hacker_Jr'>HᴀᴄKᴇʀ Jʀ</a>
 ⪼ Lɪʙʀᴀʀʏ: <a href='https://docs.pyrogram.org/'>Pʀᴏɢʀᴀᴍ</a>
 ⪼ Lᴀɴɢᴜᴀɢᴇ: <a href='https://www.python.org/'>Pʏᴛʜᴏɴ 3</a>
 ⪼ Dᴀᴛᴀ Bᴀsᴇ: <a href='https://cloud.mongodb.com/'>Mᴏɴɢᴏ Dʙ</a>
